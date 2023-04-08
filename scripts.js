b53af17d7815da5797cef3368f8cfae9459e2ad0
Toni Liungan <toniliu672@gmail.com>
2023-04-08 17:37:42 +0800

    update interface and add diagram

--- scripts.js
+++ scripts.js
@@ -37,25 +37,77 @@ function closePopup() {
 
 window.addEventListener("resize", setPopupSize);
 
-  
 
-// let popup = document.getElementById("popup");
-// let popupContent = document.getElementById("popup-content");
-// let iframe = document.getElementById("spreadsheet");
-// let spreadsheets = [
-//   "https://docs.google.com/spreadsheets/d/e/2PACX-1vTu_hHm_4GTaLlOHBILQ-xK4CnDYpO4O0Hqz-3vJ9NwoUAAv5r9U8vAxg7cUIdgSyuejdtShyiCv_DY/pubhtml?gid=1001675976&amp;single=true&amp;widget=true&amp;headers=false",
-//   "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpecI3p684bu6eF25W0DaTnGNom9Lt60XmRZFIErmPsW87ZID_prReuDe_gXM47_S3aiC2CtXUIeps/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false",
-//   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSgzBd_zAxOR3CoxoF4GsHtA9XQDlNyipNfsmLpWFbHdN7ywb94y-hP1hDmI9s1H5k1dSCYU3s2Q76I/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
-// ];
+// Diagram batang arus kas
+const cashFlowChart = new Chart(document.getElementById("cashFlowChart"), {
+  type: "bar",
+  data: {
+    labels: ["31 Maret", "1 April", "2 April", "3 April"],
+    datasets: [
+      {
+        label: "Uang Masuk (Rp)",
+        data: [40000, 100000, 40500, 570000],
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "Pengeluaran (Rp)",
+        data: [0, 100000, 0, 150000],
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "Saldo Akhir (Rp)",
+        data: [0 ,0 , 0, 500500],
+        backgroundColor: "rgba(255, 193, 7, 0.2)",
+        borderColor: "rgba(255, 193, 7, 1)",
+        borderWidth: 1,
+      },
+    ],
+  },
+});
 
-// function openPopup(sheetIndex) {
-//   iframe.src = spreadsheets[sheetIndex - 1];
-//   popup.classList.remove("hidden");
-// }
+// Diagram lingkaran pendapatan dan pengeluaran
+const incomeExpenseChart = new Chart(document.getElementById("incomeExpenseChart"), {
+  type: "doughnut",
+  data: {
+    labels: ["Pendapatan", "Pengeluaran"],
+    datasets: [
+      {
+        data: [300500, 200000], // Contoh data
+        backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
+        borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
 
-// function closePopup() {
-//   popup.classList.add("hidden");
-// }
+// efek scrolling
+// JavaScript
+$(document).ready(function () {
+  $("a.nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      const hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+
+
 
 
   
