fc177d294a06bc3b986f11a40644ddee747ddaa5
Toni Liungan <toniliu672@gmail.com>
2023-04-10 21:17:56 +0800

    Revert "update value at diagram circle 1"
    This reverts commit cbfb04f42cec7271e9427853ed58c732c79764a4.

--- scripts.js
+++ scripts.js
@@ -77,7 +77,7 @@ const incomeExpenseChart = new Chart(document.getElementById("incomeExpenseChart
     labels: ["Pendapatan", "Pengeluaran"],
     datasets: [
       {
-        data: [300500, 250000], // Contoh data
+        data: [300500, 200000], // Contoh data
         backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
         borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
         borderWidth: 1,
